39dcdb2f6a3892bec108379d4751c38e9f730ec2
Sarah Lima <lasarahlima@Sarahs-MacBook-Pro.local>
2020-06-25 22:06:44 -0300

    Reveal when the player is a spy

--- src/pages/Player/Player.js
+++ src/pages/Player/Player.js
@@ -8,16 +8,22 @@ class Player extends React.Component {
     }
   }
 
+  
+
   reveal = () => {
     this.setState({reveal: true});
   }
+
+  spyOrLocation = () => 
+    this.props.spy ? <div>You are a Spy </div> : <div>Location: {this.props.location}</div>
+  
 
   render(){
     return (
       <div>
         <div> Player 1 </div>
         <div id="reveal" onClick={this.reveal}> reveal </div>
-        { this.state.reveal && <div> Location: </div> }
+        { this.state.reveal && this.spyOrLocation()  } 
       </div>
     );
   };
--- src/pages/Player/Player.test.js
+++ src/pages/Player/Player.test.js
@@ -3,17 +3,42 @@ import React from "react";
 import Player from './Player';
 
 describe('<Player />', () => {
-    it('does not reveal the player location by default', () => {
-        const wrapper = shallow(<Player/>);
+    const spyPlayer = {
+        spy: true
+    }
+    const regularPlayer = {
+        spy:false,
+        location: "Submarine"
+    }
 
-        expect(wrapper.text()).not.toContain('Location');
+    it('does not reveal the player location or role by default', () => {
+        const wrapperRegular = shallow(<Player {...regularPlayer} />);
+        const wrapperSpy = shallow(<Player {...spyPlayer} />);
+
+        expect(wrapperRegular.text()).not.toContain('Location');
+        expect(wrapperSpy.text()).not.toContain('Spy');
     });
 
-    it('reveals the player role', () => {
-        const wrapper = shallow(<Player/>);
 
-        wrapper.find('#reveal').simulate('click');
+    describe('with a regular player', () => {
+        it('reveals the player location', () => {
+            const wrapper = shallow(<Player {...regularPlayer}/>);
+    
+            wrapper.find('#reveal').simulate('click');
+    
+            expect(wrapper.text()).toContain('Location: Submarine');
+            expect(wrapper.text()).not.toContain('Spy');
+        });
+    });
 
-        expect(wrapper.text()).toContain('Location');
+    describe('with a spy player', () => {
+        it('reveals it is a spy and does not tell a location', () => {
+            const wrapper = shallow(<Player {...spyPlayer}/>);
+    
+            wrapper.find('#reveal').simulate('click');
+    
+            expect(wrapper.text()).toContain('Spy');
+            expect(wrapper.text()).not.toContain('Location');
+        });
     });
 });
